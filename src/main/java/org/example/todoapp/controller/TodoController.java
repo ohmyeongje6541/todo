f3032b4f8cde5ab200f540d0808c60b2a1aa8de5
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-18 10:35:49 +0900

    create 로직 작성

--- src/main/java/org/example/todoapp/controller/TodoController.java
+++ src/main/java/org/example/todoapp/controller/TodoController.java
@@ -1,7 +1,11 @@
 package org.example.todoapp.controller;
 
+import org.example.todoapp.dto.TodoDto;
+import org.example.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -9,5 +13,28 @@ public class TodoController {
     @GetMapping("/todos")
     public String todos() {
         return "todos";
+    }
+
+    @GetMapping("/todos/new")
+    public String newTodo() {
+        return "new";
+    }
+
+    @GetMapping("/todos/create")
+    public String create(
+            @RequestParam String title,
+            @RequestParam String content,
+            Model model
+    ) {
+        // TodoDto 객체 생성
+        TodoDto todoDto = new TodoDto(null, title, content, false);
+
+        TodoRepository todoRepository = new TodoRepository();
+
+        TodoDto todo = todoRepository.save(todoDto);
+        model.addAttribute("todo", todo);
+        return "create";
+
+
     }
 }
--- src/main/java/org/example/todoapp/repository/TodoRepository.java
+++ src/main/java/org/example/todoapp/repository/TodoRepository.java
@@ -8,4 +8,11 @@ import java.util.concurrent.ConcurrentHashMap; // 안정성이 높음
 public class TodoRepository {
     private final Map<Long, TodoDto> storage = new ConcurrentHashMap<>();
     private Long nextId = 1L; // L = Long
+
+    public TodoDto save(TodoDto todo) {
+        // 데이터를 저장하는 로직
+        todo.setId(nextId++);
+        storage.put(todo.getId(), todo);
+        return todo;
+    }
 }
